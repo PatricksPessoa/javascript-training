abcda8545d21fa9153f0be9afb1b1d93816e78f2
patrickspessoa <patrickspessoa@gmail.com>
2020-08-01 12:42:31 -0300

    variable tipes

---
+++ variables.js
@@ -0,0 +1,6 @@
+var valor = 5; // tipo number
+var preco = 25.5; // tipo number
+var nome = "pk" // tipo string
+var idade = "21 anos" // tipo string
+var ligado = true; // tipo boolean
+var ativado = true; // tipo boolean

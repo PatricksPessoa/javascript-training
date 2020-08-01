64005357cdfcbae18a4258b92c6adb6f49d91947
patrickspessoa <patrickspessoa@gmail.com>
2020-08-01 14:21:05 -0300

    first function

---
+++ function.js
@@ -0,0 +1,12 @@
+function carro(marca, modelo, combustível, valor){
+    this.marca = ford;
+    this.modelo = siena;
+    this.combustível = gasolina;
+    this.valor = 30000;
+    this.ligar = function (){
+        return "Vrum";
+    }
+    this.buzinar = function(){
+        return "Bibi";
+    }
+}

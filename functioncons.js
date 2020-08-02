ce9654d6f03de84d0ccc64ca1baa1aab96791ef6
patrickspessoa <patrickspessoa@gmail.com>
2020-08-01 21:37:22 -0300

    java fuctioncons

---
+++ functioncons.js
@@ -0,0 +1,10 @@
+function Pessoa (nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.falar = function() {
+        return ( "Meu nome é " + this.nome + " " + this.sobrenome);
+    }
+}
+
+var programador = new Pessoa ("Patrick", "Pessoa");
+console.log(Pessoa(programador));

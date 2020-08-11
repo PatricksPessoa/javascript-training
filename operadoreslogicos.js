479222a9e159e5f39965e428f88231717c4f682f
patrickspessoa <patrickspessoa@gmail.com>
2020-08-11 17:31:18 -0300

    operadores logicos

---
+++ operadoreslogicos.js
@@ -0,0 +1,5 @@
+console.log( true && false ); // e
+console.log( false || false ); // ou
+console.log( !true ); // inverte o valor 
+console.log( 1 == '1' || "a" == "a" );
+console.log( !('a' ==1) );
---
+++ operadorrelacionais.js
@@ -0,0 +1,8 @@
+console.log (20 == 20); // igual
+console.log ( 10 != 20); // diferente
+console.log ( 10 < 20);
+console.log ( 10 === 20); // igual em valor e em tipo
+console.log ( 10 === "20");
+console.log ( 10 == "10");
+console.log (10 ===  "10");
+console.log ( 10 !== "10"); // diferente em valor e em tipo

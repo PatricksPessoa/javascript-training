d1df24f880ec23805e9a941052370843d251b73b
patrickspessoa <patrickspessoa@gmail.com>
2020-08-11 17:15:44 -0300

    operadores

---
+++ operadores.js
@@ -0,0 +1,21 @@
+var somar = 5 + 5; // adição
+var diminuir = 10 - 5; // subtração
+var multiplicar = 2 * 2.5; //multi
+var dividir = 10 / 2; //divisão
+var resto = 50 % 5; //resto da divisão
+console.log(somar);
+console.log(diminuir);
+console.log(multiplicar);
+console.log(dividir);
+console.log(resto);
+
+// conversão para string e junção
+
+console.log("texto" + " e complemento"); // texto e complemento
+console.log("5"+"6"); //56
+console.log("texto" + 20); //texto20
+console.log("texto" + true); //textotrue
+console.log("texto" + null); //textonull
+console.log("texto" + undefined); //textoundefined
+console.log([1,2,3] + 4); //1,2,34
+console.log({nome:'Patrick'} + 1); //[object Object]1

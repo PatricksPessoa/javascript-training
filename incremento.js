e374b32b00971e049dd6b8a0222f313077fcb7ce
patrickspessoa <patrickspessoa@gmail.com>
2020-08-11 17:19:45 -0300

    incremento e decremento

---
+++ incremento.js
@@ -0,0 +1,7 @@
+var number = 5;
+console.log(++number); // aumenta e depois mostra
+console.log(number++);  // mostra e só depois aumenta
+console.log(number);
+console.log(--number); // diminui e depois mostra
+console.log(number--);  // mostra e depois diminui
+console.log(number);
